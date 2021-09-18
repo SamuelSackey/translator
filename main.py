f44056e861bd4ce93900f3dd40e1f51df7d5d9f9
Samuel Sackey <smlscky@gmail.com>
2021-09-18 22:34:53 +0000

    Add translation to chinese
    Added prompt to select language

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from translate import Translator
-translator= Translator(to_lang="ja")
 
-try:
-    with open("input_file.txt", mode="r") as in_file:
-        text = in_file.read()
-        translation = translator.translate(text)
-        print(translation)
+print("Type the language to translate to. \nAvailable languages include: \n japanese \n chinese ")
 
-        with open("output_file.txt", mode="wb") as out_file:
-            out_file.write(translation.encode())
+while True:
+    set_language = input("Language:  ")
 
-except FileNotFoundError as err:
-    print("File does not exist in directory")
+    if set_language == "japanese":
+        translator= Translator(to_lang="ja")
+
+        try:
+            with open("input_file.txt", mode="r") as in_file:
+                text = in_file.read()
+                translation = translator.translate(text)
+                print(translation)
+
+                with open("output_file.txt", mode="wb") as out_file:
+                    out_file.write(translation.encode())
+
+        except FileNotFoundError as err:
+            print("File does not exist in directory")
+        break
+
+    elif set_language== "chinese":
+        translator= Translator(to_lang="zh")
+
+        try:
+            with open("input_file.txt", mode="r") as in_file:
+                text = in_file.read()
+                translation = translator.translate(text)
+                print(translation)
+
+                with open("output_file.txt", mode="wb") as out_file:
+                    out_file.write(translation.encode())
+
+        except FileNotFoundError as err:
+            print("File does not exist in directory")
+        break
+
+    else:
+        print("Error: Language is not available or does not exist")
+        continue
+
